562afcd9dcba1b6cd8c2fbb23e86a30b02bb9e3d
Mukesh <mukesh@yajtechnologies.com>
2025-03-26 09:37:58 +0545

    route fixes

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
     Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
     Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
-    Route::get('/category/{post}', [CategoryController::class, 'show'])->name('category.show');
-    Route::get('/category/{post}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::get('/category/{category}', [CategoryController::class, 'show'])->name('category.show');
+    Route::get('/category/{category}/edit', [CategoryController::class, 'edit'])->name('category.edit');
     Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
     Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
 
